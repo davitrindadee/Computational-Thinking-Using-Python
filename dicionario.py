e81ae388df5c26f9ce88fb644e33231f34291ea8
davitrindadee <davitr07@gmail.com>
2026-04-22 19:32:47 -0300

    feat : Variáveis Indexadas - Teoria (o Vetor)

---
+++ dicionario.py
@@ -0,0 +1,18 @@
+'''
+VARIÁVEIS INDEXADAS
+'''
+
+lista = (1,2,3,4,5,6,7,8,9)
+for 1 in lista:
+    print(1)
+
+listaString = ["A", "B", "C"]
+len(listaString)
+
+listaNumeros = [1, 2 ,3]
+for i in range(0, len(listaNumeros))
+print(listaNumeros[1])
+
+V = [10, 20, 30]
+
+
